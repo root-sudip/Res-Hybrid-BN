d9cace1f5ed1fe8cbf3312df9da41c1f58695483
Sudip Das <d.sudip47@gmail.com>
2019-03-27 18:17:45 +0530

    updated

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #<network filename> <training filename> <test filename> <load weight(Load/New)> <task(Train/Predict)>
-python SingleRes-Hybrid-BN.py Data/Train.h5 Data/TestGMMGC950_icdar2019.h5 Load Predict #For test
+python SingleRes-Hybrid-BN.py Data/Train.h5 Data/TestMRF950_icdar2019.h5 Load Predict #For test
 python evaluate_CER_from_prediction.py
 
